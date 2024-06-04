3ed438fc328e3651c682782708d67312aca993ab
ddiwu <wd3271391891@mail.ustc.edu.cn>
2024-06-05 00:51:15 +0800

    回归测试全部通过

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,189 +2,256 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
-#include <math.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-#define BUFSIZE 2048
+#if defined(CONFIG_ISA64)
+typedef uint64_t word_t;
+#else
+typedef uint32_t word_t;
+#endif
 
-#define MAKE_BUF \
-  int ph_width = fmt_width - arg_width; \
-  int buf_width = fmt_width>arg_width?fmt_width:arg_width; \
-  char buf[buf_width]; \
-  for (int _i = ph_width-1; _i >= 0 ; _i--) \
-    buf[_i] = placeholder; \
-  char *buf_start = buf+buf_width-arg_width
+#define NUM_LEN 32
+#define OSTRING_LEN 5120
 
-#define OUT_BUF \
-  int real_width = arg_width<n?arg_width:n; \
-  memcpy(out, buf, real_width); \
-  out += real_width; \
-  n -= real_width
+int printf(const char *fmt, ...)
+{
+  char out[OSTRING_LEN];
 
+  va_list ap;
+  va_start(ap, fmt);
+  int num = vsprintf(out, fmt, ap);
+  va_end(ap);
 
-static int width_int(int64_t n, int base) {
-    int ret = n < 0 + (base==8?1:(base==16?2:0));
-    do ++ret; while ((n /= base) != 0);
-    return ret;
+  for (int i = 0; i < num; ++i)
+  {
+    putch(out[i]);
+  }
+
+  return num;
 }
 
-static int width_uint(uint64_t n, int base) {
-    int ret = base==8?1:(base==16?2:0);
-    do ++ret; while ((n /= base) != 0);
-    return ret;
+static char *__itoa(int num, char *buff, uint16_t base)
+{
+  static const char sym[] = "0123456789abcdef";
+
+  char tmp[NUM_LEN];
+  bool is_neg = false;
+  if (num == 0)
+  {
+    strcpy(buff, "0");
+    return buff;
+  }
+  else if (num < 0)
+  {
+    is_neg = true;
+    strcpy(buff, "-");
+    buff++;
+    num = -num;
+  }
+
+  uint8_t i = 0;
+  while (num != 0)
+  {
+    tmp[i] = sym[num % base];
+    num /= base;
+    i++;
+  }
+
+  for (int j = i - 1; j >= 0; --j)
+    buff[i - 1 - j] = tmp[j];
+  buff[i] = '\0';
+
+  return is_neg ? (buff - 1) : buff;
 }
 
-static void out_int(char *out, int64_t n, int base, int width) {
-    int prefix = 0;
-    if (n < 0) {
-        out[prefix] = '-';
-        n = -n;
-        ++prefix;
+static char *__ptoa(void *p, char *buff)
+{
+  static const char sym[] = "0123456789abcdef";
+
+  word_t num = (word_t)p;
+  char tmp[NUM_LEN];
+
+  if (num == 0)
+  {
+    buff[0] = '0';
+    buff[1] = '\0';
+    return (buff - 1);
+  }
+
+  uint8_t i = 0;
+  while (num != 0)
+  {
+    tmp[i] = sym[num % 16];
+    num /= 16;
+    i++;
+  }
+
+  for (int j = i - 1; j >= 0; --j)
+    buff[i - 1 - j] = tmp[j];
+  buff[i] = '\0';
+
+  return (buff - 1);
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
+  char *pout = out;
+  size_t i = 0;
+  while (fmt[i] != '\0')
+  {
+    if (fmt[i] != '%')
+    {
+      *(pout++) = fmt[i];
     }
-    if (base == 8) {
-        out[prefix++] = '0';
-    } else if (base == 16) {
-        out[prefix++] = '0';
-        out[prefix++] = 'x';
-    }
-    prefix += base==8?1:(base==16?2:0);
-    for (int i = width-1; i >= prefix; i--, n /= base) {
-        int digit = n % base;
-        out[i] = (digit>=10)?('a'+digit-10):('0'+digit);
-    }
-}
-
-static void out_uint(char *out, uint64_t n, int base, int width) {
-    int prefix = 0;
-    if (base == 8) {
-        out[prefix++] = '0';
-    } else if (base == 16) {
-        out[prefix++] = '0';
-        out[prefix++] = 'x';
-    }
-    for (int i = width-1; i >= prefix; i--, n /= base) {
-        int digit = n % base;
-        out[i] = (digit>=10)?('a'+digit-10):('0'+digit);
-    }
-}
-
-int printf(const char *fmt, ...) {
-  char buffer[BUFSIZE];
-  va_list pArgs;
-  va_start(pArgs, fmt);
-  int ret = vsprintf(buffer, fmt, pArgs);
-  va_end(pArgs);
-  for (int i = 0; i < ret; i++) putch(buffer[i]);
-  return ret;
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  va_list pArgs;
-  va_start(pArgs, fmt);
-  int ret = vsprintf(out, fmt, pArgs);
-  va_end(pArgs);
-  return ret;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  va_list pArgs;
-  va_start(pArgs, fmt);
-  int ret = vsnprintf(out, n, fmt, pArgs);
-  va_end(pArgs);
-  return ret;
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return vsnprintf(out, UINT32_MAX, fmt, ap);
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-    char *start = out;
-    bool ps = false; // has meet percent sign '%'
-    --n; // for '\0'
-    char placeholder = ' ';
-    int fmt_width = 0;
-
-    for (; n && *fmt; ++fmt) {
-        if (!ps) {
-            if (*fmt == '%') ps = true;
-            else {
-                *out++ = *fmt;
-                --n;
-            }
-            continue;
+    else
+    {
+      bool is_end = false;
+      char num_leftpad_sym = ' ';
+      bool left_align = false;
+      int width = 0;
+      while (!is_end)
+      {
+        i++;
+        switch (fmt[i])
+        {
+        case '0':
+        {
+          if (fmt[i - 1] == '%')
+          {
+            num_leftpad_sym = '0';
+            break;
+          }
         }
-        switch (*fmt) {
-            case '%': *out++ = *fmt; --n; break;
-            case 'd': {
-                int64_t arg = va_arg(ap, int64_t);
-                int arg_width = width_int(arg, 10);
-                MAKE_BUF;
-                out_int(buf_start, arg, 10, arg_width);
-                OUT_BUF;
-                break;
-            }
-            case 'u': {
-                uint64_t arg = va_arg(ap, uint64_t);
-                int arg_width = width_uint(arg, 10);
-                MAKE_BUF;
-                out_int(buf_start, arg, 10, arg_width);
-                OUT_BUF;
-                break;
-            }
-            case 'l': { 
-                continue;
-            }
-            case 'x': case 'p': {
-                uint64_t arg = va_arg(ap, uint64_t);
-                int arg_width = width_uint(arg, 16);
-                MAKE_BUF;
-                out_uint(buf_start, arg, 16, arg_width);
-                OUT_BUF;
-                break;
-            }
-            case 's': {
-                char *arg = va_arg(ap, char*);
-                int arg_width = strlen(arg);
-                MAKE_BUF;
-                strcpy(buf_start, arg);
-                OUT_BUF;
-                break;
-            }
-            case 'c': {
-                char arg = va_arg(ap, int);
-                *out++ = arg;
-                --n;
-                break;
-            }
-            default: {
-                char c = *fmt;
-                if (c >= '0' && c <= '9') { // e.g. %09d, %9d, %6s
-                    placeholder = c=='0'?'0':' ';
-                    fmt_width = 0;
-                    if (placeholder == '0') ++fmt;
-                    while (*fmt >= '0' && *fmt <= '9') {
-                        fmt_width *= 10;
-                        fmt_width += *fmt-'0';
-                        ++fmt;
-                    }
-                    --fmt;
-                }
-                continue;
-            }
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        {
+          width = width * 10 + (fmt[i] - '0');
+          break;
         }
-        ps = false;
-        fmt_width = 0;
+        case '-':
+        {
+          if (fmt[i - 1] == '%')
+          {
+            left_align = true;
+          }
+          break;
+        }
+        case 's':
+        {
+          const char *str = va_arg(ap, char *);
+          if (!left_align)
+          {
+            for (int j = 0; j < width - (int)strlen(str); ++j)
+            {
+              *(pout++) = ' ';
+            }
+            strcpy(pout, str);
+            pout += strlen(str);
+          }
+          else
+          {
+            strcpy(pout, str);
+            pout += strlen(str);
+            for (int j = 0; j < width - (int)strlen(str); ++j)
+            {
+              *(pout++) = ' ';
+            }
+          }
+          is_end = true;
+          break;
+        }
+        case 'd':
+        case 'i':
+        case 'x':
+        {
+          int val = va_arg(ap, int);
+
+          char buff[NUM_LEN];
+          __itoa(val, buff, (fmt[i] == 'x') ? 16 : 10);
+          if (!left_align)
+          {
+            for (int j = 0; j < width - (int)strlen(buff); ++j)
+              *(pout++) = num_leftpad_sym;
+            strcpy(pout, buff);
+            pout += strlen(buff);
+          }
+          else
+          {
+            strcpy(pout, buff);
+            pout += strlen(buff);
+            for (int j = 0; j < width - (int)strlen(buff); ++j)
+              *(pout++) = num_leftpad_sym;
+          }
+          is_end = true;
+          break;
+        }
+        case 'p':
+        {
+          void *val = va_arg(ap, void *);
+
+          char buff[NUM_LEN];
+          __ptoa(val, buff);
+          if (!left_align)
+          {
+            for (int j = 0; j < width - (int)strlen(buff); ++j)
+              *(pout++) = num_leftpad_sym;
+            strcpy(pout, buff);
+            pout += strlen(buff);
+          }
+          else
+          {
+            strcpy(pout, buff);
+            pout += strlen(buff);
+            for (int j = 0; j < width - (int)strlen(buff); ++j)
+              *(pout++) = num_leftpad_sym;
+          }
+          is_end = true;
+          break;
+        }
+        default:
+        {
+          printf("print format is wrong!\n");
+          // Wrong format!!!
+          assert(0);
+        }
+        }
+      }
     }
-    *out++ = '\0';
-    return out-start;
+    ++i;
+  }
+  *pout = '\0';
+
+  va_end(ap);
+
+  return strlen(out);
 }
 
-int puts(const char * str) {
-  for (const char *c = str; *c; c++) putch(*c);
-  putch('\n');
-  return 0;
+int sprintf(char *out, const char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  int num = vsprintf(out, fmt, ap);
+  va_end(ap);
+
+  return num;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
+  panic("Not implemented");
+}
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
+{
+  panic("Not implemented");
 }
 
 #endif
--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,6 +19,7 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+void sdb_set_batch_mode();
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -29,6 +30,7 @@ int main(int argc, char *argv[]) {
 #endif
 
   /* Start engine. */
+  //sdb_set_batch_mode(); //批处理模式
   engine_start();
 
   return is_exit_status_bad();
